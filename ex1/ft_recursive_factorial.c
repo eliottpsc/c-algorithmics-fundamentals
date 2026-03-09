eb17393731ded950f2d879e73038a0a25cea7d4a
eruffin eruffin <eruffin@f2r11s3.paris.42.school>
2026-03-09 20:46:49 +0100

    fix(ex1): fix for input 0

--- ex1/ft_recursive_factorial.c
+++ ex1/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: eruffin <eruffin@learner.42.tech>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 18:37:15 by eruffin           #+#    #+#             */
-/*   Updated: 2026/03/09 19:41:42 by eruffin          ###   ########.fr       */
+/*   Updated: 2026/03/09 20:45:50 by eruffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb <= 0)
+	if (nb < 0)
 		return (0);
 	else if (nb < 2)
 		return (1);
